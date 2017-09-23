736d3808c6b6d0793c60c60394c0f41ec5c18f26
xmxshr <xmxshr@me.com>
2017-09-23 17:16:31 +0800

    基本骨架

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        {id:'1', title:'第一个list'}
+      ]
+    }
+  }
+
+  render(){
+    let todos = this.state.todoList.map((item,index)=>{
+      return <li>{item.title}</li>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>我的待办</h1>
+        <div className="input-wrapper">
+          <input type='text' value={this.state.newTodo} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ol>
+          {todos}
+        </ol>
       </div>
-    );
+    )
   }
 }
 
